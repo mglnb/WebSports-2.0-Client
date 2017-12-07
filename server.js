77c6c187d758e3aa5502855523ab190b0327094c
Miguel Boanova <mgl.deadly@gmail.com>
2017-12-07 21:02:50 -0200

    srever

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
 var port = process.env.PORT || 5000;
 app.listen(port);
 
